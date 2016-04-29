b5986949908eebe28964efa7509635ddf9cdef3b
Jervis <cuijervis@126.com>
2016-04-29 18:08:43 +0800

    add throttler module

---
+++ Presentation/Web/App_Start/Throttle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+
+namespace Web
+{
+    public class Throttle : IHttpModule
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.PostRequestHandlerExecute += OnPostRequestHandlerExecute;
+        }
+
+        /// <summary>
+        /// the media bit rate throttle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnPostRequestHandlerExecute(object sender, EventArgs e)
+        {
+            var application = (HttpApplication)sender;
+            var context = application.Context;
+            var response = context.Response;
+            //the .zip format
+            if (response.ContentType == "application/x-zip-compressed")
+            {
+                var request = context.Request;
+                if (!string.IsNullOrEmpty(request.ServerVariables["SERVER_SOFTWARE"]))
+                {
+                    //request.ServerVariables["ResponseThrottler-Enabled"] = "1";
+                    ////开始20Kb以全速下载
+                    //request.ServerVariables["ResponseThrottler-InitialSendSize"] = "1000";
+                    ////剩余部分以10Kbps速率下载，约1.25Kb/s
+                    //request.ServerVariables["ResponseThrottle-Rate"] = "10";
+
+                    context.Request.ServerVariables.Set("ResponseThrottler-InitialSendSize", "1000");
+                    context.Request.ServerVariables.Set("ResponseThrottler-Rate", "100");
+                }
+            }
+        }
+    }
+}
